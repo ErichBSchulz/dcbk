47a39cb4e2f479d50f5f5936d7a71e49f32cd695
Erich <erichbschulz@gmail.com>
2017-01-17 18:19:08 +1000

    tidy rebuild script

--- rebuild.sh
+++ rebuild.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 
-echo "Clean up old containers"
-docker rm -f buildkit
-
-echo "rebuild the image"
+echo "Rebuilding the image"
 docker build -t dcbk:v1 .
 
-echo "make a container based on the image"
+echo "Cleaning up old containers"
+docker rm -f buildkit
+echo "Making a container based on the image"
 docker create \
   -p 2222:22 \
   -p 8001:8001 \
-  -p 7979:7979 \
+  -p 80:8002 \
   --name buildkit dcbk:v1
-
 echo "starting the container"
 docker start buildkit
 
-echo "you can open a bash shell with this command:"
+echo "You can open a bash shell with this command:"
 echo "    docker exec -it buildkit bash"
+
